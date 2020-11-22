0bfcf4b8538cd097627a47fa974bbf291edd2547
CL-Kitnao <ryohei.kitano@consallink.co.jp>
2020-11-22 09:15:24 +0000

    test

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,20 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       ColorSchemer
-      <v-color-picker dot-size="25" swatches-max-height="200"></v-color-picker>
+      <v-color-picker
+        v-model="rgb"
+        dot-size="25"
+        swatches-max-height="200"
+      ></v-color-picker>
+      {{ rgb }}
     </v-col>
   </v-row>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+export default {
+  data: () => ({
+    rgb: {},
+  }),
+}
 </script>
