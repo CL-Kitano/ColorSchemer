c3d98255470a1f8980988b075618d0fa6c26b453
CL-Kitnao <ryohei.kitano@consallink.co.jp>
2020-11-22 08:56:19 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      ColorSchemer
+      <v-color-picker dot-size="25" swatches-max-height="200"></v-color-picker>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {}
+</script>
